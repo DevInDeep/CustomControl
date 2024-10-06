f137942af66e3802194bad7c87f267ee2b6f01f4
vpavlevski <vpavlevski@yahoo.com>
2024-10-06 10:57:17 +0200

    Add project files.

---
+++ CustomControl/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CustomControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gradientButton1 = new CustomControl.GradientButton();
+            this.SuspendLayout();
+            // 
+            // gradientButton1
+            // 
+            this.gradientButton1.Location = new System.Drawing.Point(67, 71);
+            this.gradientButton1.Margin = new System.Windows.Forms.Padding(1);
+            this.gradientButton1.Name = "gradientButton1";
+            this.gradientButton1.Size = new System.Drawing.Size(96, 33);
+            this.gradientButton1.TabIndex = 0;
+            this.gradientButton1.Text = "Button";
+            this.gradientButton1.Click += new System.EventHandler(this.gradientButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 189);
+            this.Controls.Add(this.gradientButton1);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GradientButton gradientButton1;
+    }
+}
+
---
+++ CustomControl/GradientButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing.Drawing2D;
+
+namespace CustomControl
+{
+    public class GradientButton : Control
+    {
+        private LinearGradientBrush _brush;
+        private readonly Font _font;
+        private string _text = string.Empty;
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [Bindable(true)]
+        public override string Text
+        {
+            get => _text; set
+            {
+                _text = value;
+                Invalidate();
+            }
+        }
+        public GradientButton()
+        {
+            _brush = new LinearGradientBrush(
+               new Point(0,10),
+               new Point(200,10),
+               Color.FromArgb(255, 255, 0, 0),
+               Color.FromArgb(255, 0, 0, 255));
+            _font = new Font(new FontFamily("Arial"), 16, FontStyle.Regular, GraphicsUnit.Pixel);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            graphics.FillRoundedRectangle(_brush, 0, 0, Width, Height, 20);
+            SizeF fontSize = graphics.MeasureString(_text, _font);
+            //Center Font
+            var x = (Width - fontSize.Width) / 2;   
+            var y = (Height - fontSize.Height) / 2;
+            graphics.DrawString(_text, _font, new SolidBrush(Color.White), new PointF(x,y));
+        }
+    }
+}
